983953e79c1be63ac92b8a6a045df2a7d9733afa
Vinicius Elias <vinelias@amazon.com>
2024-11-12 10:20:22 -0300

    feat: Added option to not use account lifecycle events to trigger the AWS Permission Set pipeline

--- modules/iam/permission-set-pipeline/locals.tf
+++ modules/iam/permission-set-pipeline/locals.tf
@@ -2,5 +2,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 locals {
-  event_rule_account_id = var.account_lifecyle_events_source == "CT" ? data.aws_organizations_organization.org.master_account_id : data.aws_ssm_parameter.aft_management_account_id[0].value
+  event_rule_account_id = var.account_lifecycle_events_source == "CT" ? data.aws_organizations_organization.org[0].master_account_id : (var.account_lifecycle_events_source == "AFT" ? data.aws_ssm_parameter.aft_management_account_id[0].value : null)
 }
